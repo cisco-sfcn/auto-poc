15ae2d7e3406abd449877be15f985c21c36abec3
sankodal <sankodal@cisco.com>
2023-01-19 17:24:06 -0600

    update tc

--- testcase/applycrd.py
+++ testcase/applycrd.py
@@ -10,17 +10,19 @@ from kubeconfig.exceptions import KubectlCommandError
 
 logger = logging.getLogger(__name__)
 
+
 class ApplyCRD(aetest.Testcase):
 
     @aetest.test
     def test(self):
         logger.info('ApplyCRD!')
         try:
-            kubectl.run(subcmd_args = "get ns")
+            kubectl.run(subcmd_args="kubectl get ns")
             logger.info('Applied resources from the config file: %s', config_path)
         except KubectlCommandError as ex:
             logger.error(ex.message)
             raise ex
+
 
 # main()
 if __name__ == '__main__':
