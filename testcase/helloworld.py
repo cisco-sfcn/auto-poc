06ac9c0192948b7a01cc7be7030904732e47b670
sankodal <sankodal@cisco.com>
2023-01-23 14:39:27 -0600

    add failure

--- testcase/helloworld.py
+++ testcase/helloworld.py
@@ -11,8 +11,17 @@ logger = logging.getLogger(__name__)
 class HelloWorld(aetest.Testcase):
 
     @aetest.test
-    def test(self):
+    def helloworld(self):
         logger.info('Hello World!')
+
+    @aetest.test
+    def byeworld(self):
+        logger.info('Bye World!')
+
+    @aetest.test
+    def failure(self):
+        logger.info('failure World!')
+        assert 1 == 0
 
 # main()
 if __name__ == '__main__':
